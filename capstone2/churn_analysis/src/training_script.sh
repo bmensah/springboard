119ecd518c13c203c41af2a468525378bcacfd2e
bmensah <b.men16@gmail.com>
2021-03-25 04:32:28 -0400

    made slight edit for clarity

--- capstone2/churn_analysis/src/training_script.sh
+++ capstone2/churn_analysis/src/training_script.sh
@@ -1,10 +1,10 @@
-BUCKET_NAME="churn-analysis-bucket"
-JOB_NAME="xgboost_$(date +"%Y%m%d_%H%M%S")"
-APP_PACKAGE_PATH="./xgboost_trainer/"
-MAIN_APP_MODULE="xgboost_trainer.training"
-RUNTIME_VERSION=2.4
-PYTHON_VERSION=3.7
-JOB_DIR=//$BUCKET_NAME/xgboost_job_dir
+BUCKET_NAME="churn-analysis-bucket" \
+JOB_NAME="xgboost_$(date +"%Y%m%d_%H%M%S")" \
+APP_PACKAGE_PATH="./xgboost_trainer/" \
+MAIN_APP_MODULE="xgboost_trainer.training" \
+RUNTIME_VERSION=2.4 \
+PYTHON_VERSION=3.7 \
+JOB_DIR=//$BUCKET_NAME/xgboost_job_dir \
 gcloud ai-platform jobs submit training $JOB_NAME \
         --package-path $APP_PACKAGE_PATH \
         --module-name $MAIN_APP_MODULE \
