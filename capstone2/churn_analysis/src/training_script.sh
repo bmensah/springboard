3f0b3336b5aaf20a40cdb65cf87959da1e2463bd
bmensah <b.men16@gmail.com>
2021-03-25 07:59:49 -0400

    correct typo in job_dir

--- capstone2/churn_analysis/src/training_script.sh
+++ capstone2/churn_analysis/src/training_script.sh
@@ -4,7 +4,7 @@ APP_PACKAGE_PATH="./xgboost_trainer/" \
 MAIN_APP_MODULE="xgboost_trainer.training" \
 RUNTIME_VERSION=2.4 \
 PYTHON_VERSION=3.7 \
-JOB_DIR=//$BUCKET_NAME/xgboost_job_dir \
+JOB_DIR=gs://$BUCKET_NAME/xgboost_job_dir \
 gcloud ai-platform jobs submit training $JOB_NAME \
         --package-path $APP_PACKAGE_PATH \
         --module-name $MAIN_APP_MODULE \
