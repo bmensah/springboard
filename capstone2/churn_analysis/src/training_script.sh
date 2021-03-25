7ec1932eddc5b0e1bcc7367f6ff3fb5257d32827
bmensah <b.men16@gmail.com>
2021-03-25 15:32:43 -0400

    fix typo in package path and module name

--- capstone2/churn_analysis/src/training_script.sh
+++ capstone2/churn_analysis/src/training_script.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 BUCKET_NAME="churn-analysis-bucket" 
 JOB_NAME="xgboost_$(date +"%Y%m%d_%H%M%S")" 
-TRAINING_PACKAGE_PATH="./xgboost_trainer/" 
+TRAINER_PACKAGE_PATH="./xgboost_trainer/" 
 MAIN_TRAINER_MODULE="xgboost_trainer.training" 
 HPTUNING_CONFIG="config.yaml"
 RUNTIME_VERSION=2.4 
 PYTHON_VERSION=3.7 
 JOB_DIR=gs://$BUCKET_NAME/xgboost_job_dir 
 gcloud ai-platform jobs submit training $JOB_NAME \
-        --package-path $APP_PACKAGE_PATH \
-        --module-name $MAIN_APP_MODULE \
+        --package-path $TRAINER_PACKAGE_PATH \
+        --module-name $MAIN_TRAINER_MODULE \
   	    --runtime-version=$RUNTIME_VERSION \
   	    --python-version=$PYTHON_VERSION \
         --job-dir $JOB_DIR \
