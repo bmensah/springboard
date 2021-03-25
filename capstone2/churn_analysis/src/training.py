e6d86c4efbae6bec3b2a1c56d98041e92d3478dd
bmensah <b.men16@gmail.com>
2021-03-25 15:18:53 -0400

    made edits to activate hyperparameter tuning

--- capstone2/churn_analysis/src/training.py
+++ capstone2/churn_analysis/src/training.py
@@ -5,7 +5,10 @@
 # Capstone 2 Project: Churn Analysis
 # Code based on the following tutorial provided by Google:
 # https://cloud.google.com/ai-platform/training/docs/getting-started-scikit-xgboost#create-training-module
-
+#
+# As well as the following tutorial by Rob Salgado:
+# https://towardsdatascience.com/hyperparameter-tuning-on-google-cloud-platform-with-scikit-learn-7d6155195efb
+#
 ##############################
 
 # [START setup]
@@ -13,8 +16,55 @@ import datetime
 import os
 import subprocess
 import sys
+import argparse
+import hypertune
 import pandas as pd
-import xgboost as xgb
+from xgboost import XGBClassifier
+from sklearn.metrics import recall_score
+from sklearn.model_selection import train_test_split
+
+
+# Create argument parser for parameters
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+        "--objective",
+        help="evaluation metric",
+        default="binary:logistic",
+        type=str
+        )
+parser.add_argument(
+       "--eta",
+       help="learning rate",
+       default=0.3,
+       type=float
+        )
+parser.add_argument(
+        "--max_depth",
+        help="tree maximum depth",
+        default=6,
+        type=int
+        )
+perser.add_argument(
+        "--min_child_weight",
+        help="minimum sum of instance weight needed in a child",
+        default=1,
+        type=int
+        )
+parder.add_argument(
+        "--colsample_bytree",
+        help="subsample ratio of columns when constructing each tree",
+        default=0.1,
+        type=float
+        )
+parder.add_argument(
+        "--gamma",
+        help="Minimum loss reduction required to make a further partition on a leaf node of the tree.",
+        default=0.1,
+        type=float
+        )
+
+args = parser.parse_args()
 
 # Google Cloud Storage bucket
 BUCKET_NAME = 'churn-analysis-bucket'
@@ -32,8 +82,8 @@ subprocess.check_call(['gsutil', 'cp', os.path.join(data_dir, target_filename), 
 
 # [START load-into-pandas]
 # Load data into pandas, then use `.values` to get NumPy arrays
-data = pd.read_csv(data_filename).values
-target = pd.read_csv(target_filename).values
+data = pd.read_csv(data_filename)#.values
+target = pd.read_csv(target_filename)#.values
 
 # Convert one-column 2D array into 1D array for use with xgboost
 target = target.reshape((target.size,))
@@ -41,10 +91,34 @@ target = target.reshape((target.size,))
 
 # [START train-and-save-model]
 # Load data into DMatrix object
-dtrain = xgb.DMatrix(data, label=target)
+#dtrain = xgb.DMatrix(data, label=target)
 
-# Train XGBoost model
-bst = xgb.train({}, dtrain, 20)
+# Split data into training and verification splits
+X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.3)
+
+# Define model with tuning params
+model = XGBClassifier(
+        objective=args.objective,
+        eta=args.eta,
+        max_depth=args.max_depth,
+        min_child_weight=args.min_child_weight,
+        colsample_bytree=args.colsample_bytree,
+        gamma=args.gamma
+        )
+#bst = xgb.train({}, dtrain, 20)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+# Define score to evaluate the classifier on recall
+score = recall_score(y_test, y_pred)
+
+# Call the hypertune library and set metric
+hpt = hypertune.HyperTune()
+hpt.report_hyperparameter_tuning_metric(
+        hyperparameter_metric_tag="recall",
+        metric_value=score,
+        global_step=1000
+        )
 
 # Export the classifier to a file
 model_filename = 'model.bst'
--- capstone2/churn_analysis/src/training_script.sh
+++ capstone2/churn_analysis/src/training_script.sh
@@ -1,10 +1,12 @@
-BUCKET_NAME="churn-analysis-bucket" \
-JOB_NAME="xgboost_$(date +"%Y%m%d_%H%M%S")" \
-APP_PACKAGE_PATH="./xgboost_trainer/" \
-MAIN_APP_MODULE="xgboost_trainer.training" \
-RUNTIME_VERSION=2.4 \
-PYTHON_VERSION=3.7 \
-JOB_DIR=gs://$BUCKET_NAME/xgboost_job_dir \
+#!/bin/bash
+BUCKET_NAME="churn-analysis-bucket" 
+JOB_NAME="xgboost_$(date +"%Y%m%d_%H%M%S")" 
+TRAINING_PACKAGE_PATH="./xgboost_trainer/" 
+MAIN_TRAINER_MODULE="xgboost_trainer.training" 
+HPTUNING_CONFIG="config.yaml"
+RUNTIME_VERSION=2.4 
+PYTHON_VERSION=3.7 
+JOB_DIR=gs://$BUCKET_NAME/xgboost_job_dir 
 gcloud ai-platform jobs submit training $JOB_NAME \
         --package-path $APP_PACKAGE_PATH \
         --module-name $MAIN_APP_MODULE \
@@ -12,4 +14,7 @@ gcloud ai-platform jobs submit training $JOB_NAME \
   	    --python-version=$PYTHON_VERSION \
         --job-dir $JOB_DIR \
         --region us-central1 \
-        --config config.yaml \
+        --config $HPTUNING_CONFIG \
+
+# Stream logs into console
+gcloud ai-platform jobs stream-logs $JOB_NAME
